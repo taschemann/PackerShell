161d96b4af6f66ff9b23ea13295ea5bd7962deca
Thomas Aschemann <taschemann@gmail.com>
2019-09-07 22:14:20 -0500

    More complexity

--- scripts/Start-DownloadMicrosoftTeams.ps1
+++ scripts/Start-DownloadMicrosoftTeams.ps1
@@ -2,21 +2,33 @@
 # Author: Thomas Aschemann
 # INSTALL PSMSI
 
-$InstallerSharePath = "\\itsys-sccm.ad.siu.edu\Software$\Microsoft\Teams"
-$temp = "$InstallerSharePath\temp"
+#$InstallerSharePath = "\\itsys-sccm.ad.siu.edu\Software$\Microsoft\Teams"
+#$temp = "$env:TEMP\teams"
+$InstallerSharePath = "$env:USERPROFILE\Desktop"
+$temp = "$env:TEMP\teams"
 $arch = @{"32" = "x86"; "64" = "x64"}
 
 if (!(Test-Path -Path "$temp")) {
     New-Item -Path "$temp" -ItemType Directory -Force
-    $arch.keys | ForEach-Object { Invoke-WebRequest -Uri $("https://aka.ms/teams{0}bitmsi" -f $_) -OutFile $("$temp\teams_{0}.msi" -f $arch[$_]) }
+}
+elseif ((Test-Path -Path "$temp") -and ((Get-ChildItem -Path $temp | Measure-Object).Count -ne 0)) {
+    Write-Warning -Message "$temp must be empty. Attempting to clean..."
+    try { Get-Childitem $temp | ForEach-Object { Remove-Item -Confirm:$false -ErrorAction Stop -Force -Recurse -Path "$temp\$_" } }
+    catch { Write-Warning $_ }
 }
 
+$arch.keys | ForEach-Object { Invoke-WebRequest -Uri $("https://aka.ms/teams{0}bitmsi" -f $_) -OutFile $("$temp\teams_{0}.msi" -f $arch[$_]) }
 $msiversion = Get-MSIProperty -Property ProductVersion -Path $temp\teams_x64.msi | Select-Object -ExpandProperty Value
 
 if (!(Test-Path -Path $InstallerSharePath\$msiversion)) {
-    $arch.Values | ForEach-Object { New-Item -Path "$InstallerSharePath\$msiversion\$_" -ItemType Directory -Force }
-    $arch.Values | ForEach-Object { Copy-Item -Path "$temp\teams_$_.msi" -Destination "$InstallerSharePath\$msiversion\$_\teams_$_.msi" }
+    foreach ($value in $arch.Values) {
+        New-Item -Path "$InstallerSharePath\$msiversion\$value" -ItemType Directory -Force
+        Copy-Item -Path "$temp\teams_$value.msi" -Destination "$InstallerSharePath\$msiversion\$value\teams_$value.msi"
+    }
 }
 
-Start-Sleep -Seconds 10
-Remove-Item -Path "$temp" -Force -Recurse
+while ((Get-ChildItem -Path "$temp" | Measure-Object).Count -ne 0) {
+    Start-Sleep -Seconds 5
+    try { Get-Childitem $temp | ForEach-Object { Remove-Item -Confirm:$false -ErrorAction Stop -Force -Recurse -Path "$temp\$_" } }
+    catch { Write-Warning $_ }
+}
