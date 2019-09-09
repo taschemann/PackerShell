cb06441d1a144dfa6734561f0aad34c1bab34208
Aschemann <taschemann@siu.edu>
2019-09-09 10:03:25 -0500

    Removed test vars and made production worthy

--- scripts/Start-DownloadMicrosoftTeams.ps1
+++ scripts/Start-DownloadMicrosoftTeams.ps1
@@ -1,26 +1,23 @@
 #!/usr/bin/env pwsh
 # Author: Thomas Aschemann
 # INSTALL PSMSI
-#$body = @"{`"text`":`"Adjutant Online. Microsoft Teams version $msiversion is available.`"}"@
 
-#$InstallerSharePath = "\\itsys-sccm.ad.siu.edu\Software$\Microsoft\Teams"
-#$temp = "$env:TEMP\teams"
-$InstallerSharePath = "$env:USERPROFILE\Desktop"
-$temp = "$env:TEMP\teams"
+$InstallerSharePath = "\\itsys-sccm.ad.siu.edu\Software$\Microsoft\Teams"
+$temp = "$env:SystemDrive\Temp"
 $arch = @{"32" = "x86"; "64" = "x64"}
-$body = @"
-    {`"text`":`"Adjutant Online. Microsoft Teams version $msiversion is available.`"}
-"@
 
 if (!(Test-Path -Path "$temp")) {
     New-Item -Path "$temp" -ItemType Directory -Force
 }
 
-arch.keys | ForEach-Object { Invoke-WebRequest -Uri $("https://aka.ms/teams{0}bitmsi" -f $_) -OutFile $("$temp\teams_{0}.msi" -f $arch[$_]) }
+$arch.keys | ForEach-Object { Invoke-WebRequest -Uri $("https://aka.ms/teams{0}bitmsi" -f $_) -OutFile $("$temp\teams_{0}.msi" -f $arch[$_]) }
 $msiversion = Get-MSIProperty -Property ProductVersion -Path $temp\teams_x64.msi | Select-Object -ExpandProperty Value
+$body = @"
+    {"text":"Adjutant Online. Microsoft Teams version $msiversion is available."}
+"@
 
 if (!(Test-Path -Path $InstallerSharePath\$msiversion)) {
-    Invoke-RestMethod -Method Post -ContentType 'Application/Json' -Body $body -Uri "https://outlook.office.com/webhook/2f9ed641-7b1f-4599-a051-ac10f7936766@d57a98e7-744d-43f9-bc91-08de1ff3710d/IncomingWebhook/86d2a7b9df98433b97940e51f80fccff/0346bb91-bfa3-462c-ae57-205a05da36eb"
+    Invoke-RestMethod -Method Post -ContentType 'Application/Json' -Body $body -Uri "https://outlook.office.com/webhook/a34d3ac2-ae62-4792-8c94-e453dd5646ce@d57a98e7-744d-43f9-bc91-08de1ff3710d/IncomingWebhook/cb5def3f8d3c43deb6429bf31e6eb41d/0346bb91-bfa3-462c-ae57-205a05da36eb"
     foreach ($value in $arch.Values) {
         New-Item -Path "$InstallerSharePath\$msiversion\$value" -ItemType Directory -Force
         do {
