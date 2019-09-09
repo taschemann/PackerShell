b0bf40e9a23dda935e79620235e77e97248f5814
Thomas Aschemann <taschemann@gmail.com>
2019-09-08 23:22:50 -0500

    Minor change

--- scripts/Start-DownloadMicrosoftTeams.ps1
+++ scripts/Start-DownloadMicrosoftTeams.ps1
@@ -1,22 +1,25 @@
 #!/usr/bin/env pwsh
 # Author: Thomas Aschemann
 # INSTALL PSMSI
+#$body = @"{`"text`":`"Adjutant Online. Microsoft Teams version $msiversion is available.`"}"@
 
 #$InstallerSharePath = "\\itsys-sccm.ad.siu.edu\Software$\Microsoft\Teams"
 #$temp = "$env:TEMP\teams"
 $InstallerSharePath = "$env:USERPROFILE\Desktop"
 $temp = "$env:TEMP\teams"
 $arch = @{"32" = "x86"; "64" = "x64"}
+$body = @"
+    {`"text`":`"Adjutant Online. Microsoft Teams version $msiversion is available.`"}
+"@
 
 if (!(Test-Path -Path "$temp")) {
     New-Item -Path "$temp" -ItemType Directory -Force
 }
 
-$arch.keys | ForEach-Object { Invoke-WebRequest -Uri $("https://aka.ms/teams{0}bitmsi" -f $_) -OutFile $("$temp\teams_{0}.msi" -f $arch[$_]) }
+arch.keys | ForEach-Object { Invoke-WebRequest -Uri $("https://aka.ms/teams{0}bitmsi" -f $_) -OutFile $("$temp\teams_{0}.msi" -f $arch[$_]) }
 $msiversion = Get-MSIProperty -Property ProductVersion -Path $temp\teams_x64.msi | Select-Object -ExpandProperty Value
 
 if (!(Test-Path -Path $InstallerSharePath\$msiversion)) {
-    $body = "{`"text`":`"Adjutant Online. Microsoft Teams version $msiversion is available.`"}"
     Invoke-RestMethod -Method Post -ContentType 'Application/Json' -Body $body -Uri "https://outlook.office.com/webhook/2f9ed641-7b1f-4599-a051-ac10f7936766@d57a98e7-744d-43f9-bc91-08de1ff3710d/IncomingWebhook/86d2a7b9df98433b97940e51f80fccff/0346bb91-bfa3-462c-ae57-205a05da36eb"
     foreach ($value in $arch.Values) {
         New-Item -Path "$InstallerSharePath\$msiversion\$value" -ItemType Directory -Force
