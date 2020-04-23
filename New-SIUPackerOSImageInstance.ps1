f773ba94465e9047d285604bdae75a01dc46db9d
Thomas Aschemann <taschemann@siu.edu>
2020-04-23 11:58:33 -0500

    Added dynamic parameters for windows configuration

--- New-SIUPackerOSImageInstance.ps1
+++ New-SIUPackerOSImageInstance.ps1
@@ -8,35 +8,80 @@ function New-SIUPackerOSImageInstance {
         [string] $OSName,
         ##
         [ValidateSet("desktop","server")]
-        [string] $BuildType,
+        [string] $BuildType
         ##
-        [Parameter(ParameterSetName="Windows")]
-        [ValidateSet("SERVERSTANDARD","SERVERSTANDARDCORE","SERVERDATACENTER","SERVERDATACENTERCORE","ENTERPRISE","PROFESSIONAL")]
-        [string] $WindowsSKU,
-        ##
-        [Parameter()]
-        [string] $VMName
     )
 
     DynamicParam {
         if ($OSName -eq 'windows') {
-            $parameter_name = "WindowsVersion"
+            if ($BuildType -eq 'server') {
 
-            $WindowsVersion = New-Object -TypeName System.Management.Automation.ParameterAttribute
-            $WindowsVersion.Position = 1
-            $WindowsVersion.Mandatory = $true
+                $param_winversion = "WindowsVersion"
+                $param_winsku = "WindowsSKU"
 
-            $validate_set_attribute = New-Object -TypeName System.Management.Automation.ValidateSetAttribute -ArgumentList '2016','2019'
-            $attribute_collection = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
-            $attribute_collection.Add($WindowsVersion)
-            $attribute_collection.Add($validate_set_attribute)
+                #WindowsVersion parameter
+                $WindowsVersion = New-Object -TypeName System.Management.Automation.ParameterAttribute
+                $WindowsVersion.Position = 1
+                $WindowsVersion.Mandatory = $true
     
-            $dynamic_parameter = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter($parameter_name, [string], $attribute_collection)
-            $parameter_dictionary = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameterDictionary
-            $parameter_dictionary.Add($parameter_name, $dynamic_parameter)
-            return $parameter_dictionary
+                $validate_set_attribute = New-Object -TypeName System.Management.Automation.ValidateSetAttribute -ArgumentList '2016','2019'
+                $attribute_collection = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
+                $attribute_collection.Add($WindowsVersion)
+                $attribute_collection.Add($validate_set_attribute)
+        
+                $dynamic_parameter = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter($param_winversion, [string], $attribute_collection)
+                $parameter_dictionary = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameterDictionary
+                $parameter_dictionary.Add($param_winversion, $dynamic_parameter)
+
+                #WindowsSKU parameter
+                $WindowsSKU = New-Object -TypeName System.Management.Automation.ParameterAttribute
+                $WindowsSKU.Position = 2
+                $WindowsSKU.Mandatory = $true
+    
+                $validate_set_attribute = New-Object -TypeName System.Management.Automation.ValidateSetAttribute -ArgumentList 'SERVERSTANDARD','SERVERSTANDARDCORE'
+                $attribute_collection = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
+                $attribute_collection.Add($WindowsSKU)
+                $attribute_collection.Add($validate_set_attribute)
+        
+                $dynamic_parameter = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter($param_winsku, [string], $attribute_collection)
+                $parameter_dictionary.Add($param_winsku, $dynamic_parameter)
+                return $parameter_dictionary
+            }
+            
+            elseif ($BuildType -eq 'desktop') {
+                $param_winversion = "WindowsVersion"
+                $param_winsku = "WindowsSKU"
+
+                #WindowsVersion parameter
+                $WindowsVersion = New-Object -TypeName System.Management.Automation.ParameterAttribute
+                $WindowsVersion.Position = 1
+                $WindowsVersion.Mandatory = $true
+    
+                $validate_set_attribute = New-Object -TypeName System.Management.Automation.ValidateSetAttribute -ArgumentList '1909','2004','LTSB2016','LTSB2019'
+                $attribute_collection = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
+                $attribute_collection.Add($WindowsVersion)
+                $attribute_collection.Add($validate_set_attribute)
+        
+                $dynamic_parameter = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter($param_winversion, [string], $attribute_collection)
+                $parameter_dictionary = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameterDictionary
+                $parameter_dictionary.Add($param_winversion, $dynamic_parameter)
+                
+                #WindowsSKU parameter
+                $WindowsSKU = New-Object -TypeName System.Management.Automation.ParameterAttribute
+                $WindowsSKU.Position = 2
+                $WindowsSKU.Mandatory = $true
+    
+                $validate_set_attribute = New-Object -TypeName System.Management.Automation.ValidateSetAttribute -ArgumentList 'ENTERPRISE','PROFESSIONAL'
+                $attribute_collection = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
+                $attribute_collection.Add($WindowsSKU)
+                $attribute_collection.Add($validate_set_attribute)
+        
+                $dynamic_parameter = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter($param_winsku, [string], $attribute_collection)
+                $parameter_dictionary.Add($param_winsku, $dynamic_parameter)
+                return $parameter_dictionary
+            }
         }
-        elseif ($OSName -eq 'ubuntu') {
+<#         elseif ($OSName -eq 'ubuntu') {
             $UbuntuVersion = New-Object -TypeName "System.Management.Automation.ParameterAttribute"
             $UbuntuVersion.ParameterSetName = "Ubuntu"
             $UbuntuVersion.Mandatory = $true
@@ -62,12 +107,13 @@ function New-SIUPackerOSImageInstance {
             $parameter_dictionary = New-Object -TypeName "System.Management.Automation.RuntimeDefinedParameterDictionary"
             $parameter_dictionary.Add("CentOSVersion", $dynamic_parameter)
             $parameter_dictionary
-        }
+        } #>
     }
     
     BEGIN {
-        $WindowsVersionObj = $PSBoundParameters[$parameter_name]
-        $WindowsVersionObj
+        # This is essential for the dynamic parameters to be recognized.
+        $WindowsVersionObj = $PSBoundParameters[$param_winversion]
+        $WindowsSkuObj = $PSBoundParameters[$param_winsku]
     }
 
     PROCESS {
@@ -75,9 +121,9 @@ function New-SIUPackerOSImageInstance {
             "windows" {
                 return $os_instance = @{
                     os_name = "$($_)"
-                    os_sku = $WindowsSKU
+                    os_sku = $WindowsSkuObj
                     os_buildtype = $BuildType
-                    vm_name = { if ($null -eq $VMName) { $VMName = "packer_$($_)_$(Get-Date -UFormat "%d%b%Y_%H%M")" } else {  } }
+                    os_version = $WindowsVersionObj
                 }
             }
         
