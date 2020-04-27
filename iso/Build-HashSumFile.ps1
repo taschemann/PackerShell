6155d0af5ccb82380177784176025bc7409a2c68
Thomas Aschemann <taschemann@gmail.com>
2020-04-27 15:42:58 -0500

    Initial commit for hash file generation script

---
+++ iso/Build-HashSumFile.ps1
@@ -0,0 +1,10 @@
+$images = Get-ChildItem -Path "$pwd" | Where-Object { $_.Extension -eq ".ISO" } | Select-Object -Property Name,BaseName,FullName,Directory
+
+foreach ($iso in $images) {
+    if (-not(Test-Path -Path "$($iso.Directory)\$($iso.BaseName)_checksum.txt")) {
+        New-Item -Path "$($iso.Directory)" -Name "$($iso.BaseName)_checksum.txt"
+        $hash = Get-FileHash -Path "$($iso.FullName)" -Algorithm SHA256
+        "$($hash.Hash)  $($iso.Name)" > "$($iso.Directory)\$($iso.BaseName)_checksum.txt"
+    }
+}
+
