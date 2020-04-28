6310d867d889966d722f8083bb2459ec8b6e222a
Thomas Aschemann <taschemann@gmail.com>
2020-04-28 12:19:45 -0500

    Added dynamic parameters for ubuntu and centos

--- Build-SIUPackerImage.ps1
+++ Build-SIUPackerImage.ps1
@@ -30,17 +30,17 @@ function Build-SIUPackerImage {
         #[ValidateSet("HyperV","Vsphere","Virtualbox")]
         #[string] $Hypervisor,
         ##
-        [Parameter(Mandatory, Position=1)]
+        [Parameter(Mandatory, Position=1, ValueFromPipeline)]
         [ValidateSet([OperatingSystemValidValues])]
         [string[]] $OSName,
         ##
-        [Parameter()]
+        [Parameter(Mandatory, Position=2, ValueFromPipelineByPropertyName)]
         [ValidateSet('desktop','server')]
         [string[]] $OSBuildType,
         ##
         [Parameter()]
         [ValidateSet("BuildBaseImage","BuildUpdatedBaseImage","CleanupBaseImage")]
-        [string] $BuildStep,
+        [string] $BuildStep = "BuildBaseImage",
         ##
         [Parameter()]
         [string] $OutputPath = "$PSScriptRoot\builds",
@@ -58,7 +58,7 @@ function Build-SIUPackerImage {
             #$param_winuanttend = "WindowsUnattendFile"
             #WindowsVersion parameter
 
-            $iso_file_names = Get-ChildItem -Path ".\iso\" | Where-Object { $_.Name -like "windows*.iso" }
+            $iso_file_names = Get-ChildItem -Path "$PSScriptRoot\iso" | Where-Object { $_.Name -like "$OSName*.iso" }
             $iso_table = [ordered] @{}
             $iso_table_array = @()
             foreach ($file in $iso_file_names) {
@@ -102,12 +102,21 @@ function Build-SIUPackerImage {
         elseif ($OSName -eq 'ubuntu') {
             $param_ubuntuversion = "UbuntuVersion"
 
+            $iso_file_names = Get-ChildItem -Path "$PSScriptRoot\iso" | Where-Object { $_.Name -like "$OSName*.iso" }
+            $iso_table = [ordered] @{}
+            $iso_table_array = @()
+            foreach ($file in $iso_file_names) {
+                $iso_properties = $file.BaseName -split '-'
+                $iso_table = (ConvertFrom-StringData -StringData "OSName = $($iso_properties[0]) `n OSVersion = $($iso_properties[1]) `n OSType = $($iso_properties[2]) `n OSArch = $($iso_properties[3])")
+                $iso_table_array += ($iso_table)
+            }
+
             #UbuntuVersion parameter
             $UbuntuVersion = New-Object -TypeName System.Management.Automation.ParameterAttribute
             $UbuntuVersion.Position = 1
             $UbuntuVersion.Mandatory = $true
 
-            $validate_set_attribute = New-Object -TypeName System.Management.Automation.ValidateSetAttribute -ArgumentList '18.04','20.04'
+            $validate_set_attribute = New-Object -TypeName System.Management.Automation.ValidateSetAttribute -ArgumentList $($iso_table_array | Where-Object { $_.OSType -eq $OSBuildType} | Select-Object -ExpandProperty OSVersion)
             $attribute_collection = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
             $attribute_collection.Add($UbuntuVersion)
             $attribute_collection.Add($validate_set_attribute)
@@ -115,21 +124,35 @@ function Build-SIUPackerImage {
             $dynamic_parameter = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter($param_ubuntuversion, [string], $attribute_collection)
             $parameter_dictionary = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameterDictionary
             $parameter_dictionary.Add($param_ubuntuversion, $dynamic_parameter)
+
             return $parameter_dictionary
         }
         elseif ($OSName -eq "centos") {
-            $CentOSVersion = New-Object -TypeName "System.Management.Automation.ParameterAttribute"
-            $CentOSVersion.ParameterSetName = "CentOS"
+            $param_centosversion = "CentOSVersion"
+
+            $iso_file_names = Get-ChildItem -Path "$PSScriptRoot\iso" | Where-Object { $_.Name -like "$OSName*.iso" }
+            $iso_table = [ordered] @{}
+            $iso_table_array = @()
+            foreach ($file in $iso_file_names) {
+                $iso_properties = $file.BaseName -split '-'
+                $iso_table = (ConvertFrom-StringData -StringData "OSName = $($iso_properties[0]) `n OSVersion = $($iso_properties[1]) `n OSType = $($iso_properties[2]) `n OSArch = $($iso_properties[3])")
+                $iso_table_array += ($iso_table)
+            }
+
+            #CentOSVersion parameter
+            $CentOSVersion = New-Object -TypeName System.Management.Automation.ParameterAttribute
+            $CentOSVersion.Position = 1
             $CentOSVersion.Mandatory = $true
-            $CentOSVersion.ValidateSet("7","8")
-    
-            $attribute_collection = New-Object -TypeName "System.Collections.ObjectModel.Collection[System.Attribute]"
+
+            $validate_set_attribute = New-Object -TypeName System.Management.Automation.ValidateSetAttribute -ArgumentList $($iso_table_array | Where-Object { $_.OSType -eq 'server'} | Select-Object -ExpandProperty OSVersion)
+            $attribute_collection = New-Object -TypeName System.Collections.ObjectModel.Collection[System.Attribute]
             $attribute_collection.Add($CentOSVersion)
+            $attribute_collection.Add($validate_set_attribute)
     
-            $dynamic_parameter = New-Object -TypeName "System.Management.Automation.RuntimeDefinedParameter(`"CentOSVersion`", [string], $attribute_collection)"
-            $parameter_dictionary = New-Object -TypeName "System.Management.Automation.RuntimeDefinedParameterDictionary"
-            $parameter_dictionary.Add("CentOSVersion", $dynamic_parameter)
-            $parameter_dictionary
+            $dynamic_parameter = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameter($param_centosversion, [string], $attribute_collection)
+            $parameter_dictionary = New-Object -TypeName System.Management.Automation.RuntimeDefinedParameterDictionary
+            $parameter_dictionary.Add($param_centosversion, $dynamic_parameter)
+            return $parameter_dictionary
         }
     }
 
@@ -137,7 +160,7 @@ function Build-SIUPackerImage {
         $iso_directory = "$PSScriptRoot\iso"
         Invoke-Expression -Command "$iso_directory\Build-HashSumFile.ps1"
         #Regenerate the iso table
-        $iso_file_names = Get-ChildItem -Path ".\iso\" | Where-Object { $_.Name -like "windows*.iso" }
+        $iso_file_names = Get-ChildItem -Path "$PSScriptRoot\iso\" | Where-Object { $_.Name -like "windows*.iso" }
         $iso_table = [ordered] @{}
         $iso_table_array = @()
         foreach ($file in $iso_file_names) {
@@ -150,11 +173,18 @@ function Build-SIUPackerImage {
             $WindowsVersionObj = $PSBoundParameters[$param_winversion]
             $WindowsSkuObj = $PSBoundParameters[$param_winsku]
             $CurrentWindowsObject = $iso_table_array | Where-Object { $_.OSVersion -eq $WindowsVersionObj }
-            $CurrentWindowsObject
         }
-        elseif ($iso_table_array.OSName -eq 'ubuntu') {
+        elseif ($OSName -eq 'ubuntu') {
             # This is essential for the dynamic parameters to be recognized.
-            $UbuntuVersionObj = $PSBoundParameters[$param_ubuntuversion]         
+            $UbuntuVersionObj = $PSBoundParameters[$param_ubuntuversion]
+        }
+        elseif ($OSName -eq 'centos') {
+            # This is essential for the dynamic parameters to be recognized.
+            $CentOSVersionObj = $PSBoundParameters[$param_centosversion]
+
+            if ($OSBuildType -eq 'desktop') {
+                throw "Desktop deployment not available for CentOS right now. Please use `"server`" for your OSBuildType value."
+            }
         }
     }
     
@@ -191,10 +221,7 @@ function Build-SIUPackerImage {
                         vm_name = "$VMName"
                         build_type = "$BuildType"
                         iso_url = "$IsoUrl"
-                        unattend_file = "$unattend_path"
-                        cpu = $ProcessorCount
-                        ram_size = $MemoryInMegabytes
-                        disk_size = $DiskSizeInMegabytes
+                        kickstart_file = "$unattend_path"
                         output_directory = "$OutputPath"
                     }
                 }
@@ -206,7 +233,7 @@ function Build-SIUPackerImage {
                         vm_name = "$VMName"
                         build_type = "$BuildType"
                         iso_url = "$IsoUrl"
-                        unattend_file = "$unattend_path"
+                        preseed_file = "$unattend_path"
                         cpu = $ProcessorCount
                         ram_size = $MemoryInMegabytes
                         disk_size = $DiskSizeInMegabytes
