ca72e1dc927cc75dc8fbb0e7e311c45f8d4f9524
Thomas Aschemann <taschemann@gmail.com>
2020-05-04 18:13:58 -0500

    Initial commit for answer.iso creation script

---
+++ windows/unattend/Build-WindowsAnswerImage.ps1
@@ -0,0 +1,13 @@
+#Script used to create answer.iso files for secondary_iso_images
+#Author: Thomas Aschemann
+
+$unattend_root = "$PSScriptRoot"
+$unattend_files = Get-ChildItem -Recurse $unattend_root -File | Where-Object {$_.Directoryname -ne "$unattend_root"}
+
+if (($IsWindows) -and ($env:PROCESSOR_ARCHITECTURE -eq "amd64")) {
+    $build_utils = "..\Deployment Tools\amd64"
+
+    foreach ($file in $unattend_files) {
+        Start-Process -FilePath "$build_utils\Oscdimg\oscdimg.exe" -ArgumentList "-lWIN_ANSWER_ISO -m -u2 $($file.DirectoryName) $($file.DirectoryName)\answer.iso"
+    }
+}
