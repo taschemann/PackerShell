b932cde97e3b1a71d6486ff2430625f18433fe1c
Thomas Aschemann <taschemann@gmail.com>
2020-05-22 13:25:19 -0500

    Fixed paths

--- Build-WindowsAnswerImage.ps1
+++ Build-WindowsAnswerImage.ps1
@@ -1,11 +1,12 @@
 #Script used to create answer.iso files for secondary_iso_images
 #Author: Thomas Aschemann
 
-$unattend_root = "$PSScriptRoot"
+$packer_root = "$PSScriptRoot"
+$unattend_root = "$packer_root\unattend"
 $unattend_files = Get-ChildItem -Recurse $unattend_root -File | Where-Object {($_.Directoryname -ne "$unattend_root") -and ($_.Extension -ne ".iso")}
 
 if (($IsWindows) -and ($env:PROCESSOR_ARCHITECTURE -eq "amd64")) {
-    $build_utils = "..\Deployment Tools\amd64"
+    $build_utils = "$packer_root\bin\Deployment Tools\amd64"
 
     if (-not (Test-Path "$build_utils\Oscdimg\oscdimg.exe")) {
         throw "oscdimg.exe not found. Please install the Windows Deployment Tools from the Windows Assessment and Deployment Toolkit."
